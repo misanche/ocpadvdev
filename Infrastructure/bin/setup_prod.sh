ed8ef94fa3c16ece4239e36012fb6f349b73e829
Mikel Sanchez <misanche@redhat.com>
2018-09-05 09:27:40 +0200

    Added mlbparks prod setup

--- Infrastructure/bin/setup_prod.sh
+++ Infrastructure/bin/setup_prod.sh
@@ -22,3 +22,19 @@ oc policy add-role-to-user view --serviceaccount=default
 
 echo "Create MongoDB"
 oc new-app -f ./Infrastructure/templates/mongodb_prod.yaml
+echo "Create ConfiMaps"
+oc create configmap mongodb-configmap --from-literal DB_HOST=mongodb --from-literal DB_PORT=27017 --from-literal DB_USERNAME=mongodb --from-literal DB_PASSWORD=mongodb --from-literal DB_NAME=parks --from-literal DB_REPLICASET=rs0
+oc create configmap mlbparks-green-configmap --from-literal APPNAME="MLB Parks (Green)"
+oc create configmap mlbparks-blue-configmap --from-literal APPNAME="MLB Parks (Blue)"
+oc create configmap nationalparks-green-configmap --from-literal APPNAME="National Parks (Green)"
+oc create configmap nationalparks-blue-configmap --from-literal APPNAME="National Parks (Blue)"
+oc create configmap parksmap-green-configmap --from-literal APPNAME="ParksMap (Green)"
+oc create configmap parksmap-blue-configmap --from-literal APPNAME="ParksMap (Blue)"
+
+echo "Crate mlbparks App"
+echo "Create Green"
+oc new-app -l 7077-parks-prod/mlbparks:0.0 app=mlbparks-green --allow-missing-imagestream-tags=true --allow-missing-images=true --name=mlbparks-green
+oc rollout cancel dc/mlbparks-green
+echo "Create Blue"
+oc new-app -l 7077-parks-prod/mlbparks:0.0 app=mlbparks-blue  --allow-missing-imagestream-tags=true --name=mlbparks-blue
+oc rollout cancel dc/mlbparks-blue
