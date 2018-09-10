2aa6ebc6e08f123bf06423f8512823f99db20f5c
Mikel Sanchez <misanche@redhat.com>
2018-09-10 16:07:58 +0200

    check earlier

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -34,8 +34,6 @@ oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n
 oc policy add-role-to-user admin system:serviceaccount:gpte-jenkins:jenkins -n ${GUID}-jenkins
 # Create the Jenkins app
 oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi -n ${GUID}-jenkins
-# Create custom Jenkins Slave pod
-cat ./Dockerfile | oc new-build --name=jenkins-slave-appdev --dockerfile=- -n ${GUID}-jenkins
 
 while : ; do
     echo "Checking if Jenkins is Ready..."
@@ -44,6 +42,9 @@ while : ; do
     echo "...no. Sleeping 10 seconds."
     sleep 10
 done
+# Create custom Jenkins Slave pod
+cat ./Dockerfile | oc new-build --name=jenkins-slave-appdev --dockerfile=- -n ${GUID}-jenkins
+
 #echo "Create pipelines for mlbparks, nationalparks and parksmap"
 #oc create -f ./Infrastructure/templates/pipelines/mlbparks-pipeline.yaml 
 #oc create -f ./Infrastructure/templates/pipelines/nationalparks-pipeline.yaml 
