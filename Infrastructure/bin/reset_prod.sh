b7ee99adb0d56e92fd1bf9983ab49ef6b897a071
Mikel Sanchez <misanche@redhat.com>
2018-09-10 12:38:19 +0200

    no need patching

--- Infrastructure/bin/reset_prod.sh
+++ Infrastructure/bin/reset_prod.sh
@@ -18,6 +18,6 @@ echo "Resetting Parks Production Environment in project ${GUID}-parks-prod to Gr
 # rollout followed by a Green rollout.
 
 # To be Implemented by Student
-oc patch route mlbparks -n ${GUID}-parks-prod -p '{"spec":{"to":{"name":"mlbparks-green"}}}'
-oc patch route nationalparks -n ${GUID}-parks-prod -p '{"spec":{"to":{"name":"nationalparks-green"}}}'
-oc patch route parksmap -n ${GUID}-parks-prod -p '{"spec":{"to":{"name":"parksmap-green"}}}'
+oc patch route mlbparks -n ${GUID}-parks-prod -p '{"spec":{"to":{"name":"mlbparks-green"}}}' || echo "no need to patch mlbparks"
+oc patch route nationalparks -n ${GUID}-parks-prod -p '{"spec":{"to":{"name":"nationalparks-green"}}}' || echo "no need to patch nationalparks"
+oc patch route parksmap -n ${GUID}-parks-prod -p '{"spec":{"to":{"name":"parksmap-green"}}}' || echo "no need to patch parksmap"
